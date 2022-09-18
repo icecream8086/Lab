4ce100b2ecf3b55039bae19af8546264c6e61377
LinLai <zzbsn.cn@outlook.com>
2022-09-18 19:30:24 +0800

    update file

--- start_Github_SSL&Proxy.bat
+++ start_Github_SSL&Proxy.bat
@@ -5,8 +5,6 @@
 @REM 测试版文件
 
 git config --global http.sslVerify "false"
-git config --global https.proxy
-git config --global --unset https.proxy
 git config --global https.proxy http://127.0.0.1:1080 
 git config --global http.proxy http://127.0.0.1:1080
 git config --global --unset http.proxy
